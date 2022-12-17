131e831e6c63eb8c63d376caca7e35a91e183b20
Raghu Mina <raghumina11@gmail.com>
2022-12-18 00:04:37 +0530

    Update contacts_model.dart

--- contact_app/lib/UI/model/contacts_model.dart
+++ contact_app/lib/UI/model/contacts_model.dart
@@ -1,12 +1,15 @@
+import 'package:faker/faker.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:faker/faker.dart' as faker;
+
 import '../../data/contact.dart';
 
 class ContactsModel extends Model {
+  // ignore: prefer_final_fields
   late List<Contact> _contacts = List.generate(50, (index) {
     return Contact(
-      name: faker.person.firstName() + ' ' + faker.person.lastName(),
-      email: faker.internet.freeEmail(),
+      name: faker.Person().firstName() + ' ' + faker.Person().lastName(),
+      email: faker.Internet().freeEmail(),
       phoneNumber: random.integer(1000000).toString(),
     );
   });
@@ -15,14 +18,15 @@ class ContactsModel extends Model {
 
   void changeFavouriteStatus(int index) {
     _contacts[index].isFavorite = !_contacts[index].isFavorite;
-    _contacts[index].isFavourite = !_contacts[index].isFavourite;
-                _contacts.sort((a, b) {
-                  if (a.isFavourite) {
-                    return -1;
-                  } else if (b.isFavourite) {
-                    return 1;
-                  } else {
-                    return 0;
-                  }
-                });
+
+    _contacts.sort((a, b) {
+      if (a.isFavourite) {
+        return -1;
+      } else if (b.isFavourite) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
 }
