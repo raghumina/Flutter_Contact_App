0c730575c2c8bd331d0b92521b91924abf1acf28
Raghu Mina <raghumina11@gmail.com>
2022-12-23 17:07:16 +0530

    Making list items slidable

--- contact_app/lib/UI/widget/contact_tile.dart
+++ contact_app/lib/UI/widget/contact_tile.dart
@@ -1,7 +1,9 @@
 import 'package:contact_app/UI/contact/contact_edit_page.dart';
 import 'package:contact_app/UI/model/contacts_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:scoped_model/scoped_model.dart';
+//import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ContactTile extends StatelessWidget {
   const ContactTile({
@@ -14,27 +16,38 @@ class ContactTile extends StatelessWidget {
   Widget build(BuildContext context) {
     final model = ScopedModel.of<ContactModel>(context);
     final displayedContact = model.contacts[contactIndex];
-    return ListTile(
-      title: Text(displayedContact.name!),
-      subtitle: Text(displayedContact.email),
-      trailing: IconButton(
-          icon: Icon(
-            displayedContact.isFavourite ? Icons.star : Icons.star_border,
-            color: displayedContact.isFavourite
-                ? Colors.amber
-                : Color.fromARGB(255, 255, 68, 68),
-          ),
-          onPressed: () {
-            model.changeFavoriteStatus(contactIndex);
-          }),
-      onTap: () {
-        Navigator.of(context).push(MaterialPageRoute(
-          builder: (_) => ContactEditPage(
-            editedContact: displayedContact,
-            editedContactIndex: contactIndex,
-          ),
-        ));
-      },
+
+    return Slidable(
+      endActionPane: ActionPane(motion: const BehindMotion(), children: [
+        SlidableAction(
+          icon: Icons.delete,
+          label: 'Delete',
+          backgroundColor: Colors.red,
+          onPressed: ((context) => {}),
+        )
+      ]),
+      child: ListTile(
+        title: Text(displayedContact.name!),
+        subtitle: Text(displayedContact.email),
+        trailing: IconButton(
+            icon: Icon(
+              displayedContact.isFavourite ? Icons.star : Icons.star_border,
+              color: displayedContact.isFavourite
+                  ? Colors.amber
+                  : Color.fromARGB(255, 255, 68, 68),
+            ),
+            onPressed: () {
+              model.changeFavoriteStatus(contactIndex);
+            }),
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (_) => ContactEditPage(
+              editedContact: displayedContact,
+              editedContactIndex: contactIndex,
+            ),
+          ));
+        },
+      ),
     );
   }
 }
